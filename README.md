ed597e5e1e09e5727a579082a4e80871d0d77a93
Denver <denvercowan@rocketmail.com>
2022-07-04 14:41:52 -0400

    finished project and added readme content

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# odin-rock-paper-scissors-project
+# The Odin Project
+
+## Rock Paper Scissors project
+
+### Skills I learned or got better with during this project:
+
+-Attaching JS to html files in head and body. if using in head use keyword defer
+-Using functions to really break the problem down into the smallest component parts so that at the end all thats left to do is make a single function call that handles the rest.
+
+### Future Plans
+
+1. Make a GUI for the application.
+2. Make a game tracker that keeps up with wins and losses until the browser window is closed.
--- main.js
+++ main.js
@@ -19,12 +19,46 @@ let computerSelection = computerPlay();
 //function and logic to play a single round
 //must use backticks in return statements to reference variable like this ${playerScore}
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "Rock" && computerSelection == "Scissors") {
+  if (playerSelection == "rock" && computerSelection == "Scissors") {
     playerScore++;
     return `You win rock beats scissors! Score: ${playerScore} to ${computerScore}`;
-  } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+  } else if (playerSelection == "rock" && computerSelection == "Paper") {
     computerScore++;
     return `You lose paper beats rock. Score: ${playerScore} to ${computerScore}`;
+  } else if (playerSelection == "paper" && computerSelection == "Rock") {
+    playerScore++;
+    return `You win paper beats rock. Score: ${playerScore} to ${computerScore};`;
+  } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+    computerScore++;
+    return `You lose scissors beats paper. Score: ${playerScore} to ${computerScore}`;
+  } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+    playerScore++;
+    return `You win scissors beats paper. Score: ${playerScore} to ${computerScore}`;
+  } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+    computerScore++;
+    return `You lose rock beats scissors. Score: ${playerScore} to ${computerScore}`;
   } else return `That was a draw. Score: ${playerScore} to ${computerScore}`;
 }
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection)); //check to make sure logic is working correctly
+
+// logic for playing a five round game and allowing the user to select their choice each round.
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Type Rock, Paper, or Scissors");
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  if (playerScore > computerScore) {
+    console.log(`You won! final Score: ${playerScore} to ${computerScore}`);
+  } else if (computerScore > playerScore) {
+    console.log(`You lose! final score: ${playerScore} to ${computerScore}`);
+  } else {
+    console.log(`You tied :( final score: ${playerScore} to ${computerScore}`);
+  }
+}
+game();
+
+/* Game works as desired. I would like to eventually come back and add a gui.
+The logic for this was not too complicated and was good practice for JS syntax and rules.
+*/
